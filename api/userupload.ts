a9772fd5ce58863932fc66e5208b6e50aae00d40
Waritthon2004 <Waritthonzaza@gmail.com>
2024-03-17 05:59:22 +0700

    Update userupload.ts

--- api/userupload.ts
+++ api/userupload.ts
@@ -69,7 +69,7 @@ router.post("/",fileupload.diskLoader.single("file"),async(req,res)=>{
         last_idx: result.insertId,
       });
       const currentDate =  getCurrentDate();
-      console.log(currentDate);
+      //console.log(currentDate);
       
       let check2: any = await new Promise((resolve, reject) => {
         conn.query("INSERT INTO `Statics`(`PID`, `Date`, `point`) VALUES (?,?,?)",[result.insertId, currentDate,0], (err, result) => {
