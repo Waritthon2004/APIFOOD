40a67f68ec58c4f3770340bbebfa986b1693125f
Khanomtom <146657653+Khanomtom@users.noreply.github.com>
2024-03-24 21:28:55 +0700

    lol

--- api/standding.ts
+++ api/standding.ts
@@ -38,3 +38,20 @@ router.get("/", (req, res) => {
     });
   });
 
+  router.get("/admin", (req, res) => {
+    let sql = `SELECT PID, point, url, ROW_NUMBER() OVER (ORDER BY point DESC) AS "rank" 
+    FROM (
+        SELECT Picture.PID, MAX(Statics.point) AS point, MAX(url) AS url 
+        FROM Statics, Picture 
+        WHERE Picture.PID = Statics.PID AND Statics.Date = (SELECT MAX(Date) FROM Statics) GROUP BY 	  	Picture.PID 
+    ) 
+    AS max_points ORDER BY point DESC `;
+    conn.query(sql, (err, result) => {
+      if (err) throw err;
+      res
+        .status(200)
+        .json(result);
+    });
+  });
+
+
